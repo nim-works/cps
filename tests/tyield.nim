7902b091e4e762cf59335cbb675e2667bcc33db1
Andy Davidoff <andy+github@disruptek.com>
2020-07-22 18:02:14 -0400

    add yield test

---
+++ tests/tyield.nim
@@ -0,0 +1,31 @@
+import std/os
+
+import cps
+import cps/eventqueue
+
+const
+  start = 2
+  tiny = 0
+  big = start * 2
+var count = start
+
+proc higher(ms: int): Cont {.cps.} =
+  while count < big and count > tiny:
+    inc count
+    cps_sleep ms
+    cps_yield()
+    cps_yield()
+    cps_yield()
+
+proc lower(ms: int): Cont {.cps.} =
+  while count < big and count > tiny:
+    dec count
+    cps_sleep ms
+    cps_yield()
+
+trampoline higher(1)
+trampoline lower(1)
+
+run()
+
+assert count == tiny
