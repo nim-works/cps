06a59f29b82978e0defe0c920b34abfe3101f9b3
Andy Davidoff <andy+github@disruptek.com>
2020-07-22 16:13:47 -0400

    simplify

--- cps.nimble
+++ cps.nimble
@@ -12,10 +12,12 @@ proc execTest(test: string) =
   execCmd "nim c           -f -r " & test
   execCmd "nim c   -d:release -r " & test
   execCmd "nim c   -d:danger  -r " & test
+  execCmd "nim c             --gc:arc -r " & test
+  execCmd "nim c   -d:danger --gc:arc -r " & test
   execCmd "nim cpp            -r " & test
   execCmd "nim cpp -d:danger  -r " & test
-  execCmd "nim c   --gc:arc -r " & test
-  execCmd "nim cpp --gc:arc -r " & test
+  execCmd "nim cpp           --gc:arc -r " & test
+  execCmd "nim cpp -d:danger --gc:arc -r " & test
 
 task test, "run tests for travis":
   execTest("tests/test.nim")
--- tests/tock.nim
+++ tests/tock.nim
@@ -1,22 +1,20 @@
-import std/times
-
 import cps             # .cps. macro
 import cps/eventqueue  # cps_sleep(), trampoline, run(), Cont
 
 # a procedure that starts off synchronous and becomes asynchronous
-proc tock(name: string; interval: Duration): Cont {.cps.} =
-  var count: int = 20
+proc tock(name: string; ms: int): Cont {.cps.} =
+  var count: int = 10
   while count > 0:
     dec count
     # this primitive sends the continuation to the dispatcher
-    cps_sleep interval
+    cps_sleep ms
     # this is executed from the dispatcher
     echo name, " ", count
 
 # the trampoline repeatedly invokes continuations...
-trampoline tock("tick", initDuration(milliseconds = 300))
+trampoline tock("tick", 30)
 # ...until they complete or are queued in the dispatcher
-trampoline tock("tock", initDuration(seconds = 1))
+trampoline tock("tock", 100)
 
 # run the dispatcher to invoke pending continuations
 run()
