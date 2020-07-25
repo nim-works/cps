af2dbcbba13ff59df101fd986378304c228d2642
Andy Davidoff <andy+github@disruptek.com>
2020-07-25 13:43:19 -0400

    add another broken test

--- tests/tblock.nim
+++ tests/tblock.nim
@@ -14,3 +14,17 @@ trampoline test()
 if r != 3:
   echo "r was ", r
   quit(1)
+
+proc test2(): Cont {.cps.} =
+  r = 1
+  block:
+    if true:
+      cps jield()
+      inc r
+      break
+    quit(1)
+  inc r
+trampoline test()
+if r != 3:
+  echo "r was ", r
+  quit(1)
