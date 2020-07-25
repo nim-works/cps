0eabbea8a3ae2498cea3c78235ea4b45cbe749f3
Andy Davidoff <andy+github@disruptek.com>
2020-07-25 11:22:06 -0400

    yet more fun

---
+++ tests/twhile.nim
@@ -0,0 +1,24 @@
+import std/unittest
+
+import cps
+import cps/eventqueue
+
+proc adder(x: var int) =
+  inc x
+
+suite "cps":
+
+  setup:
+    var cup: int
+
+  test "while":
+    proc foo(): Cont {.cps.} =
+      var i: int = 0
+      while i < 2:
+        let x: int = i
+        adder(i)
+        assert x < i
+        check x < i
+      cup = i
+      check cup == 2
+    trampoline foo()
