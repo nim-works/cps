337274651f44629c398adc889a7da03e52fbbee3
Ico Doornekamp <git@zevv.nl>
2020-07-27 20:14:56 +0200

    Example code comments

--- stash/iterator.nim
+++ stash/iterator.nim
@@ -1,28 +1,39 @@
 
+#
+# This is an example implementation for a basic CPS-based iterator.
+#
+
 import cps, options
+
+# This is our iterator type. It holds the continuation function
+# and an Option[int] to pass the last produced value
 
 type Iterator = ref object of RootObj
   fn*: proc(c: Iterator): Iterator {.nimcall.}
   val: Option[int]
 
 
+# The `produce` proc is called to pump the iterator. It will trampoline the
+# continuation until a value is available in `val`.
+
 proc produce(c: var Iterator): Option[int] =
   while c != nil and c.fn != nil and c.val.isNone:
     c = c.fn(c)
   if c != nil and c.val.isSome:
-    let val = c.val
+    result = c.val
     c.val = none(int)
-    return val
-  else:
-    return none(int)
 
+
+# The `jield` proc is cps magic to generate a new value from within an
+# interator
 
 proc jield(c: Iterator, val: int): Iterator =
   c.val = some(val)
   return c
 
 
-# Counting iterator
+# A simple counting iterator, will produce all integers from 'lo' to 'high',
+# inclusive
 
 proc counter(lo: int, hi: int): Iterator {.cps.} =
   var i:int = lo
@@ -30,12 +41,18 @@ proc counter(lo: int, hi: int): Iterator {.cps.} =
     cps jield(i)
     inc i
 
-# Resume the iterator a bunch of times
+
+# Create an instance of the iterator, counting from 3 up to 7
 
 var a = counter(3, 7)
+
+
+# Resume the iterator a bunch of times
+
 echo "produced ", a.produce()
 echo "produced ", a.produce()
 echo "produced ", a.produce()
 echo "produced ", a.produce()
 echo "produced ", a.produce()
 echo "produced ", a.produce()
+
