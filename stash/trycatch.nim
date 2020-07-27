612871cb626b388118a58d071e2a13b7dd241123
Ico Doornekamp <git@zevv.nl>
2020-07-27 23:52:31 +0200

    Added trycatch.nim

---
+++ stash/trycatch.nim
@@ -0,0 +1,63 @@
+
+#
+# Proof of concept try/catch/throw
+#
+
+import cps
+
+
+type
+  Tc = ref object of RootObj
+    fn*: proc(c: Tc): Tc {.nimcall.}
+
+var
+  err: bool
+  msg: string
+  where: Tc
+
+# CPS magic functions
+
+proc start(c: Tc): Tc =
+  where = c
+  return c
+
+proc throw(c: Tc, m: string): Tc =
+  err = true
+  msg = m
+  return where
+
+
+# This is a cps func that will throw an error
+
+proc deeper(): Tc {.cps.} =
+  echo "throwing"
+  cps throw("Boom")
+  echo "you will never see this"
+
+
+# 'main' cps function, sets up try/cach and calls another
+# function that will throw
+
+proc foo(): Tc {.cps.} =
+  
+  cps start()
+
+  if not err:
+    echo "one"
+    echo "two"
+    return deeper()
+    echo "three"
+    echo "four"
+  
+  if err:
+    echo "** caught error: ", msg, " **"
+
+  echo "bye"
+
+
+
+# Trampoline
+
+var c = foo()
+while c != nil and c.fn != nil:
+  c = c.fn(c)
