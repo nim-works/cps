9fc6bd0193c70c83451c9af28c304343ba652670
Ico Doornekamp <ico@pruts.nl>
2020-08-05 19:40:28 +0200

    Added iteratorT

--- stash/iteratorT.nim
+++ stash/iteratorT.nim
@@ -8,25 +8,25 @@ import cps, options
 # This is our iterator type. It holds the continuation function
 # and an Option[int] to pass the last produced value
 
-type Iterator = ref object of RootObj
-  fn*: proc(c: Iterator): Iterator {.nimcall.}
-  val: Option[int]
+type Iterator[T] = ref object of RootObj
+  fn*: proc(c: Iterator[T]): Iterator[T] {.nimcall.}
+  val: Option[T]
 
 # The `produce` proc is called to pump the iterator. It will trampoline the
 # continuation until a value is available in `val`.
 
-proc produce(c: var Iterator): Option[int] =
+proc produce[T](c: var Iterator[T]): Option[T] =
   while c != nil and c.fn != nil and c.val.isNone:
     c = c.fn(c)
   if c != nil and c.val.isSome:
     result = c.val
-    c.val = none(int)
+    c.val = none(T)
 
 
 # The `jield` proc is cps magic to generate a new value from within an
 # interator
 
-proc jield(c: Iterator, val: int): Iterator =
+proc jield[T](c: Iterator[T], val: T): Iterator[T] =
   c.val = some(val)
   return c
 
@@ -34,8 +34,8 @@ proc jield(c: Iterator, val: int): Iterator =
 # A simple counting iterator, will produce all integers from 'lo' to 'high',
 # inclusive
 
-proc counter(lo: int, hi: int) {.cps:Iterator.} =
-  var i:int = lo
+proc counter[T](lo: T, hi: T) {.cps:Iterator[T].} =
+  var i: T = lo
   while i <= hi:
     cps jield(i)
     inc i
@@ -43,7 +43,7 @@ proc counter(lo: int, hi: int) {.cps:Iterator.} =
 
 # Create an instance of the iterator, counting from 3 up to 7
 
-var a = counter(3, 7)
+var a = counter[int](3, 7)
 
 
 # Resume the iterator a bunch of times
