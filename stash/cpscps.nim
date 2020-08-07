b46b6efb714be188892a7bd82e791fc73ef77ba8
Andy Davidoff <andy+github@disruptek.com>
2020-08-07 12:15:44 -0400

    use eventqueue

--- stash/cpscps.nim
+++ stash/cpscps.nim
@@ -1,29 +1,35 @@
 
 import cps
+import cps/eventqueue
 
 # Middleware
 
-type C = ref object of RootObj
-  fn*: proc(c: C): C {.nimcall.}
+when false:
+  type C = ref object of RootObj
+    fn*: proc(c: C): C {.nimcall.}
 
-proc sleep(c: C): C =
-  echo "sleep"
-  return c
+  proc sleep(c: C): C =
+    echo "sleep"
+    return c
+
+else:
+  type C = Cont
 
 # User code
 
 proc two() {.cps:C.} =
   echo "two a"
-  cps sleep()
+  cps sleep(2000)
   echo "two b"
 
 proc one() {.cps:C.} =
   echo "one a"
-  return two()    # <== HERE
+  cps two()
   echo "one b"
 
 
 var c = one()
 
-while c != nil and c.fn != nil:
-  c = c.fn(c)
+run()
+#while c != nil and c.fn != nil:
+#  c = c.fn(c)
