73e339440e416135f0bd2bfe31f74f1943cf92cc
Andy Davidoff <andy+github@disruptek.com>
2020-08-14 15:47:25 -0400

    make it fail better

--- tests/tock.nim
+++ tests/tock.nim
@@ -5,10 +5,10 @@ proc tock(name: string; ms: int) {.cps: Cont.} =
   var count: int = 10
   while count > 0:
     dec count
-    yield sleep(ms)
+    sleep(ms)
     echo name, " ", count
 
-trampoline tock("tick", 300)
-trampoline tock("tock", 700)
+tock("tick", 300)
+tock("tock", 700)
 
 run()
