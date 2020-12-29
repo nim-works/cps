13495fbe1449313cc9e61b523e15730076f98ea5
Andy Davidoff <github@andy.disruptek.com>
2020-12-29 11:26:34 -0500

    make taste tests build

--- tests/taste.nim
+++ tests/taste.nim
@@ -19,7 +19,6 @@ testes:
     trampoline foo()
     check j == 4
 
-when false:
   block trampoline:
     r = 0
     proc foo() {.cps: Cont.} =
@@ -39,7 +38,6 @@ when false:
         k == 3
     trampoline foo()
 
-when false:
   block assignment_shim:
     r = 0
     proc bar(a: int): int {.cps: Cont.} =
@@ -68,7 +66,7 @@ when false:
         adder(i)
       r = i
       check r == 3
-    foo_clyybber()
+    trampoline foo()
 
   block:
     ## shadowing and proc param defaults
@@ -90,7 +88,7 @@ when false:
           a == 5
           b == 7
           c == 3
-      foo_clyybber(1, 2)
+      trampoline foo(1, 2)
 
   block:
     ## reassignment of var proc params
@@ -105,7 +103,7 @@ when false:
         b == 7
         c == 3
     var (x, y, z) = (1, 2, 3)
-    foo_clyybber(x, y, z)
+    trampoline foo(x, y, z)
 
   block:
     ## multiple variable declaration
@@ -124,18 +122,15 @@ when false:
       inc p
       inc q
       inc r
-    foo_clyybber()
+    trampoline foo()
 
   block:
     ## declaration without type
-    when true:
-      skip("broken until cps macro is typed")
-    else:
-      proc foo() {.cps: Cont.} =
-        var j = 2
-        noop()
-        check j == 2
-      foo_clyybber()
+    proc foo() {.cps: Cont.} =
+      var j = 2
+      noop()
+      check j == 2
+    trampoline foo()
 
   block:
     ## simple block with break
@@ -145,12 +140,12 @@ when false:
         if true:
           inc r
           break
-        check false
+        fail()
       inc r
-    foo_clyybber()
+    trampoline foo()
     if r != 3:
       checkpoint "r wasn't 3: ", r
-      check false
+      fail()
 
   block:
     ## block with break
@@ -161,12 +156,12 @@ when false:
           noop()
           inc r
           break
-        check false
+        fail()
       inc r
-    foo_clyybber()
+    trampoline foo()
     if r != 3:
       checkpoint "r wasn't 3: ", r
-      check false
+      fail()
 
   block:
     ## semaphores
@@ -181,8 +176,8 @@ when false:
       wait(sem)
       success = true
 
-    signalSleeper_clyybber(10)
-    signalWaiter_clyybber()
+    trampoline signalSleeper(10)
+    trampoline signalWaiter()
 
     run()
 
@@ -213,9 +208,9 @@ when false:
           break
         inc r
         if r > 2:
-          check false
+          fail()
         return
-    foo_clyybber()
+    trampoline foo()
     check r == 1, "r was " & $r
 
   block:
@@ -227,11 +222,11 @@ when false:
         if true:
           inc r
           if r > 2:
-            check false
+            fail()
           else:
             break
       inc r
-    foo_clyybber()
+    trampoline foo()
     check r == 3, "r was " & $r
 
   block:
@@ -245,13 +240,14 @@ when false:
             break found
           noop()
           inc r
-        check false
+        fail()
       r = r * -1
-    foo_clyybber()
+    trampoline foo()
     check r == -3, "r was " & $r
 
   block:
     ## while statement
+    fail"infinite loop"
     proc foo() {.cps: Cont.} =
       var i: int = 0
       while i < 2:
@@ -260,7 +256,7 @@ when false:
         check x < i
       r = i
       check r == 2
-    foo_clyybber()
+    trampoline foo()
 
   block:
     ## while with break
@@ -274,10 +270,11 @@ when false:
         check x < i
       r = i
       check r == 2
-    foo_clyybber()
+    trampoline foo()
 
   block:
     ## while with continue
+    fail"infinite loop"
     proc foo() {.cps: Cont.} =
       var i: int = 0
       while i < 2:
@@ -288,7 +285,7 @@ when false:
         check x > 0
       r = i
       check r == 2
-    foo_clyybber()
+    trampoline foo()
 
   block:
     ## simple name shadowing test
@@ -332,19 +329,19 @@ when false:
         if r == 5:
           break
       inc r
-    foo_clyybber()
+    trampoline foo()
     check r == 6, "r is " & $r
 
   block:
     ## fork
-    when not declaredInScope(fork):
-      skip("fork() not declared")
+    when not defined(fork):
+      skip"fork() not declared"
     else:
       proc foo() {.cps: Cont.} =
         fork()
         inc r
 
-      foo_clyybber()
+      trampoline foo()
       if r != 2:
         raise newException(Defect, "uh oh")
 
@@ -357,14 +354,14 @@ when false:
         sleep(ms)
         checkpoint name, " ", count
 
-    foo_clyybber("tick", 3)
-    foo_clyybber("foo", 7)
+    foo("tick", 3)
+    foo("foo", 7)
     run()
 
   block:
     ## shadow mission impossible
     when true:
-      skip("will not work until new scopes go in")
+      skip"will not work until new scopes go in"
     else:
       proc b(x: int) {.cps: Cont.} =
         noop()
@@ -436,212 +433,212 @@ when false:
         sleep(ms)
         jield()
 
-    higher(1)
-    lower(1)
+    trampoline higher(1)
+    trampoline lower(1)
     run()
 
     if count != tiny:
       raise newException(ValueError, "you're a terrible coder")
 
-when false:
-  import epoll
-  import posix
-  import tables
-  import deques
-  import os
+    when false:
+      import epoll
+      import posix
+      import tables
+      import deques
+      import os
 
-  testes:
-    block:
-      ## zevv's echo service
+      testes:
+        block:
+          ## zevv's echo service
 
-      proc timerfd_create(clock_id: ClockId, flags: cint): cint
-         {.cdecl, importc: "timerfd_create", header: "<sys/timerfd.h>".}
+          proc timerfd_create(clock_id: ClockId, flags: cint): cint
+             {.cdecl, importc: "timerfd_create", header: "<sys/timerfd.h>".}
 
-      proc timerfd_settime(ufd: cint, flags: cint,
-                            utmr: ptr Itimerspec, otmr: ptr Itimerspec): cint
-         {.cdecl, importc: "timerfd_settime", header: "<sys/timerfd.h>".}
+          proc timerfd_settime(ufd: cint, flags: cint,
+                                utmr: ptr Itimerspec, otmr: ptr Itimerspec): cint
+             {.cdecl, importc: "timerfd_settime", header: "<sys/timerfd.h>".}
 
-      type
+          type
 
-        Cont = ref object of RootObj
-          fn*: proc(c: Cont): Cont {.nimcall.}
+            Cont = ref object of RootObj
+              fn*: proc(c: Cont): Cont {.nimcall.}
 
-        Evq = ref object
-          epfd: cint
-          work: Deque[Cont]
-          fds: Table[cint, Cont]
-          running: bool
+            Evq = ref object
+              epfd: cint
+              work: Deque[Cont]
+              fds: Table[cint, Cont]
+              running: bool
 
-        Timer = proc()
+            Timer = proc()
 
-      ## Event queue implementation
+          ## Event queue implementation
 
-      proc newEvq(): Evq =
-        new result
-        result.epfd = epoll_create(1)
+          proc newEvq(): Evq =
+            new result
+            result.epfd = epoll_create(1)
 
-      proc stop(evq: Evq) =
-        evq.running = false
+          proc stop(evq: Evq) =
+            evq.running = false
 
-      proc addWork(evq: Evq, cont: Cont) =
-        evq.work.addLast cont
+          proc addWork(evq: Evq, cont: Cont) =
+            evq.work.addLast cont
 
-      proc addFd(evq: Evq, fd: SocketHandle | cint, cont: Cont) =
-        evq.fds[fd.cint] = cont
+          proc addFd(evq: Evq, fd: SocketHandle | cint, cont: Cont) =
+            evq.fds[fd.cint] = cont
 
-      proc delFd(evq: Evq, fd: SocketHandle | cint) =
-        evq.fds.del(fd.cint)
+          proc delFd(evq: Evq, fd: SocketHandle | cint) =
+            evq.fds.del(fd.cint)
 
-      proc io(evq: Evq, c: Cont, fd: SocketHandle | cint, event: int): Cont =
-        var epv = EpollEvent(events: event.uint32)
-        epv.data.u64 = fd.uint
-        discard epoll_ctl(evq.epfd, EPOLL_CTL_ADD, fd.cint, epv.addr)
-        evq.addFd(fd, c)
+          proc io(evq: Evq, c: Cont, fd: SocketHandle | cint, event: int): Cont =
+            var epv = EpollEvent(events: event.uint32)
+            epv.data.u64 = fd.uint
+            discard epoll_ctl(evq.epfd, EPOLL_CTL_ADD, fd.cint, epv.addr)
+            evq.addFd(fd, c)
 
-      proc sleep(evq: Evq, c: Cont, timeout: int): Cont =
-        let fd = timerfd_create(CLOCK_MONOTONIC, 0)
-        var ts: Itimerspec
-        ts.it_interval.tv_sec = Time(timeout div 1_000)
-        ts.it_interval.tv_nsec = (timeout %% 1_000) * 1_000_000
-        ts.it_value.tv_sec = ts.it_interval.tv_sec
-        ts.it_value.tv_nsec = ts.it_interval.tv_nsec
-        check timerfd_settime(fd.cint, 0.cint, ts.addr, nil) != -1
-        evq.io(c, fd, POLLIN)
+          proc sleep(evq: Evq, c: Cont, timeout: int): Cont =
+            let fd = timerfd_create(CLOCK_MONOTONIC, 0)
+            var ts: Itimerspec
+            ts.it_interval.tv_sec = Time(timeout div 1_000)
+            ts.it_interval.tv_nsec = (timeout %% 1_000) * 1_000_000
+            ts.it_value.tv_sec = ts.it_interval.tv_sec
+            ts.it_value.tv_nsec = ts.it_interval.tv_nsec
+            check timerfd_settime(fd.cint, 0.cint, ts.addr, nil) != -1
+            evq.io(c, fd, POLLIN)
 
-      proc run(evq: Evq) =
-        evq.running = true
-        while true:
+          proc run(evq: Evq) =
+            evq.running = true
+            while true:
 
-          # Pump the queue until empty
-          while evq.work.len > 0:
-            let c = evq.work.popFirst
-            let c2 = c.fn(c)
-            if c2 != nil:
-              evq.addWork c2
+              # Pump the queue until empty
+              while evq.work.len > 0:
+                let c = evq.work.popFirst
+                let c2 = c.fn(c)
+                if c2 != nil:
+                  evq.addWork c2
 
-          if not evq.running:
-            break
+              if not evq.running:
+                break
 
-          # Wait for all registered file descriptors
-          var events: array[8, EpollEvent]
-          let n = epoll_wait(evq.epfd, events[0].addr, events.len.cint, 1000)
+              # Wait for all registered file descriptors
+              var events: array[8, EpollEvent]
+              let n = epoll_wait(evq.epfd, events[0].addr, events.len.cint, 1000)
 
-          # Put continuations for all ready fds back into the queue
-          for i in 0..<n:
-            let fd = events[i].data.u64.cint
-            evq.addWork evq.fds[fd]
-            evq.delFd(fd)
-            discard epoll_ctl(evq.epfd, EPOLL_CTL_DEL, fd.cint, nil)
+              # Put continuations for all ready fds back into the queue
+              for i in 0..<n:
+                let fd = events[i].data.u64.cint
+                evq.addWork evq.fds[fd]
+                evq.delFd(fd)
+                discard epoll_ctl(evq.epfd, EPOLL_CTL_DEL, fd.cint, nil)
 
-      ## Some convenience functions to hide the dirty socket stuff, this
-      ## keeps the CPS functions as clean and readable as possible
+          ## Some convenience functions to hide the dirty socket stuff, this
+          ## keeps the CPS functions as clean and readable as possible
 
-      proc sockBind(port: int): SocketHandle =
-        let fds = posix.socket(AF_INET, SOCK_STREAM, 0)
-        var sas: Sockaddr_in
-        sas.sin_family = AF_INET.uint16
-        sas.sin_port = htons(port.uint16)
-        sas.sin_addr.s_addr = INADDR_ANY
-        var yes: int = 1
-        check setsockopt(fds, SOL_SOCKET, SO_REUSEADDR, yes.addr, sizeof(yes).SockLen) != -1
-        check bindSocket(fds, cast[ptr SockAddr](sas.addr), sizeof(sas).SockLen) != -1
-        check listen(fds, SOMAXCONN) != -1
-        return fds
+          proc sockBind(port: int): SocketHandle =
+            let fds = posix.socket(AF_INET, SOCK_STREAM, 0)
+            var sas: Sockaddr_in
+            sas.sin_family = AF_INET.uint16
+            sas.sin_port = htons(port.uint16)
+            sas.sin_addr.s_addr = INADDR_ANY
+            var yes: int = 1
+            check setsockopt(fds, SOL_SOCKET, SO_REUSEADDR, yes.addr, sizeof(yes).SockLen) != -1
+            check bindSocket(fds, cast[ptr SockAddr](sas.addr), sizeof(sas).SockLen) != -1
+            check listen(fds, SOMAXCONN) != -1
+            return fds
 
-      proc sockAccept(fds: SocketHandle): SocketHandle =
-        var sac: Sockaddr_in
-        var sacLen: SockLen
-        let fdc = posix.accept(fds, cast[ptr SockAddr](sac.addr), sacLen.addr)
-        check fcntl(fdc, F_SETFL, fcntl(fdc, F_GETFL, 0) or O_NONBLOCK) != -1
-        return fdc
+          proc sockAccept(fds: SocketHandle): SocketHandle =
+            var sac: Sockaddr_in
+            var sacLen: SockLen
+            let fdc = posix.accept(fds, cast[ptr SockAddr](sac.addr), sacLen.addr)
+            check fcntl(fdc, F_SETFL, fcntl(fdc, F_GETFL, 0) or O_NONBLOCK) != -1
+            return fdc
 
-      proc sockRecv(fd: SocketHandle): string =
-        result = newString(1024)
-        let n = posix.recv(fd, result[0].addr, result.len, 0)
-        if n >= 0:
-          result.setlen(n)
-        else:
-          result.setlen(0)
+          proc sockRecv(fd: SocketHandle): string =
+            result = newString(1024)
+            let n = posix.recv(fd, result[0].addr, result.len, 0)
+            if n >= 0:
+              result.setlen(n)
+            else:
+              result.setlen(0)
 
-      proc sockSend(fd: SocketHandle, s: string) =
-        let n = posix.send(fd, s[0].unsafeAddr, s.len, 0)
-        check(n == s.len)
+          proc sockSend(fd: SocketHandle, s: string) =
+            let n = posix.send(fd, s[0].unsafeAddr, s.len, 0)
+            check(n == s.len)
 
-      proc sockConnect(address: string, port: int): SocketHandle =
-        discard
-        let fd = posix.socket(AF_INET, SOCK_STREAM, 0)
-        var sas: Sockaddr_in
-        sas.sin_family = AF_INET.uint16
-        sas.sin_port = htons(port.uint16)
-        sas.sin_addr.s_addr = inet_addr(address)
-        var yes: int = 1
-        check connect(fd, cast[ptr SockAddr](sas.addr), sizeof(sas).SockLen) != -1
-        return fd
+          proc sockConnect(address: string, port: int): SocketHandle =
+            discard
+            let fd = posix.socket(AF_INET, SOCK_STREAM, 0)
+            var sas: Sockaddr_in
+            sas.sin_family = AF_INET.uint16
+            sas.sin_port = htons(port.uint16)
+            sas.sin_addr.s_addr = inet_addr(address)
+            var yes: int = 1
+            check connect(fd, cast[ptr SockAddr](sas.addr), sizeof(sas).SockLen) != -1
+            return fd
 
-      var evq = newEvq()
-      var count = 0
-      var clients = 0
+          var evq = newEvq()
+          var count = 0
+          var clients = 0
 
-      ## CPS server session hander
-      proc handleClient(fdc: SocketHandle) {.cps: Cont.} =
+          ## CPS server session hander
+          proc handleClient(fdc: SocketHandle) {.cps: Cont.} =
 
-        inc clients
+            inc clients
 
-        while true:
-          evq.io(fdc, POLLIN)
-          let s: string = sockRecv(fdc)
-          if s.len == 0: break
-          inc count
-          evq.io(fdc, POLLOUT)
-          sockSend(fdc, s)
+            while true:
+              evq.io(fdc, POLLIN)
+              let s: string = sockRecv(fdc)
+              if s.len == 0: break
+              inc count
+              evq.io(fdc, POLLOUT)
+              sockSend(fdc, s)
 
-        dec clients
-        discard fdc.close()
-
-
-      ## CPS server listener handler
-      proc doEchoServer(port: int) {.cps: Cont.} =
-        let fds: SocketHandle = sockBind(port)
-        checkpoint "listening fd: ", fds.int
-        while true:
-          evq.io(fds, POLLIN)
-          let fdc: SocketHandle = sockAccept(fds)
-          #checkpoint "accepted fd:", fdc.int
-          # Create new client and add to work queue
-          evq.addWork handleClient(fdc)
+            dec clients
+            discard fdc.close()
 
 
-      ## CPS client handler
-      proc doEchoClient(address: string, port: int, n: int, msg: string) {.cps: Cont.} =
-        let fd: SocketHandle = sockConnect(address, port)
-        #checkpoint "connected fd: ", fd.int
+          ## CPS server listener handler
+          proc doEchoServer(port: int) {.cps: Cont.} =
+            let fds: SocketHandle = sockBind(port)
+            checkpoint "listening fd: ", fds.int
+            while true:
+              evq.io(fds, POLLIN)
+              let fdc: SocketHandle = sockAccept(fds)
+              #checkpoint "accepted fd:", fdc.int
+              # Create new client and add to work queue
+              evq.addWork handleClient(fdc)
 
-        var i: int = 0
-        while i < n:
-          evq.io(fd, POLLOUT)
-          sockSend(fd, msg)
-          evq.io(fd, POLLIN)
-          let msg2: string = sockRecv(fd)
-          check msg2 == msg
-          inc i
 
-        discard fd.close()
-        #checkpoint "disconnected fd: ", fd.int
+          ## CPS client handler
+          proc doEchoClient(address: string, port: int, n: int, msg: string) {.cps: Cont.} =
+            let fd: SocketHandle = sockConnect(address, port)
+            #checkpoint "connected fd: ", fd.int
 
-      ## Progress reporting
-      proc doTicker() {.cps: Cont.} =
-        while true:
-          evq.sleep(1000)
-          checkpoint "tick. clients: ", clients, " echoed ", count, " messages"
-          if clients == 0:
-            evq.stop()
+            var i: int = 0
+            while i < n:
+              evq.io(fd, POLLOUT)
+              sockSend(fd, msg)
+              evq.io(fd, POLLIN)
+              let msg2: string = sockRecv(fd)
+              check msg2 == msg
+              inc i
 
-      ## Spawn workers
-      evq.addWork doTicker()
-      evq.addWork doEchoServer(8000)
-      for i in 1..100:
-        evq.addWork doEchoClient("127.0.0.1", 8000,
-                                 2000, "The quick brown fox jumped over the lazy dog")
+            discard fd.close()
+            #checkpoint "disconnected fd: ", fd.int
 
-      ## Forever run the event queue
-      evq.run()
+          ## Progress reporting
+          proc doTicker() {.cps: Cont.} =
+            while true:
+              evq.sleep(1000)
+              checkpoint "tick. clients: ", clients, " echoed ", count, " messages"
+              if clients == 0:
+                evq.stop()
+
+          ## Spawn workers
+          evq.addWork doTicker()
+          evq.addWork doEchoServer(8000)
+          for i in 1..100:
+            evq.addWork doEchoClient("127.0.0.1", 8000,
+                                     2000, "The quick brown fox jumped over the lazy dog")
+
+          ## Forever run the event queue
+          evq.run()
