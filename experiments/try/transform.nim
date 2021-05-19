76a4fb262957ab97b995b5bfaaf2885a1db6f54a
Andy Davidoff <github@andy.disruptek.com>
2021-05-18 23:04:25 -0400

    slightly more automated via templates

--- experiments/try/transform.nim
+++ experiments/try/transform.nim
@@ -12,6 +12,24 @@ proc noop(c: C): C =
   echo "noop"
   c
 
+template post_exception_body(goto: typed; body: untyped) {.dirty.} =
+  if getCurrentException().isNil:
+    body
+  c.fn = goto
+
+template sensitive_exit {.dirty.} =
+  if c.fn.isNil and not getCurrentException().isNil:
+    raise
+  return c
+
+template catch_and_go(ex: untyped; goto: typed; body: untyped) {.dirty.} =
+  try:
+    body
+    c.fn = goto
+  except CpsException as `ex`:
+    c.`ex` = (ref CatchableError)(`ex`)
+    c.fn = goto
+
 proc f2(c: C): C =
   ## finally from the "toplevel" try
 
@@ -27,15 +45,12 @@ proc f2(c: C): C =
     setCurrentException c.e # added boilerplate
 
   inc r
-  c.fn = nil
 
-  if getCurrentException().isNil:
+  post_exception_body nil:
     # this would be the body after the toplevel try statement
     discard
 
-  if c.fn.isNil and not getCurrentException().isNil:
-    raise
-  return c
+  sensitive_exit()
 
 proc b2(c: C): C =
   # we look for non-nil exceptions
@@ -47,29 +62,15 @@ proc b2(c: C): C =
     setCurrentException c.e # added boilerplate
 
   # it's in a try so we need to catch it
-  try:
+  catch_and_go x2, f2:
     inc r
     doAssert getCurrentExceptionMsg() == "something"
 
-    if getCurrentException().isNil:
-
+    post_exception_body f2:
       # proceed into the toplevel try body
       doAssert false, "this should not run"
 
-    # go to toplevel finally
-    c.fn = f2
-
-  except CpsException as x2:
-    # we catch exceptions here for (at least) two reasons:
-    # - we know we may have a finally to execute,
-    # - we need to set the exception correctly,
-    c.x2 = (ref CatchableError)(x2)
-    # go to toplevel finally
-    c.fn = f2
-
-  if c.fn.isNil and not getCurrentException().isNil:
-    raise
-  return c
+  sensitive_exit()
 
 proc f1(c: C): C =
   ## finally from the "middle" try
@@ -82,61 +83,29 @@ proc f1(c: C): C =
     # finally when exception was thrown
     setCurrentException c.e # added boilerplate
 
-  # it's in a try so we need to catch it
-  try:
+  catch_and_go x2, f2:
     # finally body starts with a noop, so ...
     c.fn = b2
     return noop c
 
-  except CpsException as x2:
-    # we catch exceptions here for (at least) two reasons:
-    # - we know we may have a finally to execute,
-    # - we need to set the exception correctly,
-    c.x2 = (ref CatchableError)(x2)
-
-    # go to toplevel finally
-    c.fn = f2
-
-  if c.fn.isNil and not getCurrentException().isNil:
-    raise
-  return c
+  sensitive_exit()
 
 proc b(c: C): C =
   ## inner-most try body successful; this is the code
   ## after that try statement
 
-  # we know we're in a try, which means /we/ have to catch
-  # any exceptions for those clauses, or any finally
-
-  try:
-
+  catch_and_go x1, f1:
     # user code for body after the try
     raise newException(ValueError, "something")
 
-    # we will definitely travel to the "middle" finally now
-    c.fn = f1
-
-  except CpsException as x1:
-    # we catch exceptions here for (at least) two reasons:
-    # - we know we may have a finally to execute,
-    # - we need to set the exception correctly,
-    c.x1 = (ref CatchableError)(x1)
-    # we will definitely travel to the "middle" finally now
-    c.fn = f1
-
-  if c.fn.isNil and not getCurrentException().isNil:
-    raise
-  return c
+  sensitive_exit()
 
 proc b1(c: C): C =
   # interior of clause
   if not c.e.isNil:
     setCurrentException c.e  # added boilerplate
 
-  # we know we're in a try, which means /we/ have to catch
-  # any exceptions for those clauses, or any finally
-
-  try:
+  catch_and_go x1, f1:
     # user code for clause
     inc r
     doAssert c.e.msg == "some error"
@@ -144,19 +113,7 @@ proc b1(c: C): C =
     # after the try
     raise newException(ValueError, "something")
 
-    # we will definitely travel to the "middle" finally now
-    c.fn = f1
-  except CpsException as x1:
-    # we catch exceptions here for (at least) two reasons:
-    # - we know we may have a finally to execute,
-    # - we need to set the exception correctly,
-    c.x1 = (ref CatchableError)(x1)
-    # we will definitely travel to the "middle" finally now
-    c.fn = f1
-
-  if c.fn.isNil and not getCurrentException().isNil:
-    raise
-  return c
+  sensitive_exit()
 
 proc foo(): C =
   var c = C()
