bf810f4fc6bd23836615865e4d75040d950b98d3
Ico Doornekamp <ico@pruts.nl>
2021-05-20 11:47:16 +0200

    coro cleanup

--- stash/coroutine.nim
+++ stash/coroutine.nim
@@ -9,37 +9,39 @@ type
     c: Cont
     sIn: string
 
-  CoroFn = proc(coro: Coro): Cont
+  CoroFn = proc(app: App, coro: Coro): Cont
+
+  App = ref object
+    coro1: Coro
+    coro2: Coro
 
 
 proc jield(c: Cont, coro: Coro): Cont {.cpsMagic.} =
   coro.c = c
-
-proc send(coro: Coro, s: string) =
-  coro.sIn = s
-
-proc recv(coro: Coro): string =
-  coro.sIn
  
 proc resume(coro: Coro) =
   var c = coro.c
   while c.running:
     c = c.fn(c)
 
-proc newCoro(fn: CoroFn): Coro =
+proc send(coro: Coro, s: string) =
+  coro.sIn = s
+  coro.resume()
+
+proc recv(coro: Coro): string =
+  coro.sIn
+
+proc newCoro(app: App, fn: CoroFn): Coro =
   let coro = Coro()
-  coro.c = fn(coro)
+  coro.c = fn(app, coro)
   coro.resume()
   coro
 
 
-var coro1: Coro
-var coro2: Coro
-
 
 # This coro receives strings, accumulates and splits by newline,
-# and resumes coro 2
-proc fn_coro1(coro: Coro) {.cps:Cont} =
+# and send the lines to coro2
+proc fn_coro1(app: App, coro: Coro) {.cps:Cont} =
   var buf = ""
   while true:
     coro.jield()
@@ -47,31 +49,29 @@ proc fn_coro1(coro: Coro) {.cps:Cont} =
     for c in s:
       if c == '\n':
         echo "tx ", buf
-        coro2.send(buf)
-        coro2.resume()
+        app.coro2.send(buf)
         buf = ""
       else:
         buf = buf & c
 
 # This coro receives strings from coro1 and prints them
-proc fn_coro2(coro: Coro) {.cps:Cont} =
+proc fn_coro2(app: App, coro: Coro) {.cps:Cont} =
   while true:
     coro.jield()
     let s = coro.recv()
     echo "rx ", s
 
 
-coro1 = newCoro(fn_coro1)
-coro1.resume()
+let app = App()
 
-coro2 = newCoro(fn_coro2)
-coro2.resume()
+app.coro1 = app.newCoro(fn_coro1)
+app.coro1.resume()
+
+app.coro2 = app.newCoro(fn_coro2)
+app.coro2.resume()
 
 while true:
-  coro1.send("line ")
-  coro1.resume()
-  coro1.send("one\nline ")
-  coro1.resume()
-  coro1.send("two\n")
-  coro1.resume()
+  app.coro1.send("line ")
+  app.coro1.send("one\nline ")
+  app.coro1.send("two\n")
 
