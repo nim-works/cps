7934c6da8f62ed82efe9e1c0c5c4c293cf25e0d8
Saem Ghani <saemghani+github@gmail.com>
2021-05-29 18:30:38 -0700

    y u no work?

--- cps/rewrites.nim
+++ cps/rewrites.nim
@@ -77,9 +77,9 @@ proc normalizingRewrites*(n: NimNode): NimNode =
       if n.kind == nnkIdentDefs:
         if n.len == 2:
           n.add newEmptyNode()
-        elif n[1].isEmpty:          # add explicit type symbol
-          n[1] = getTypeInst n[2]
-        n[2] = normalizingRewrites n[2]
+        elif n[^2].isEmpty:          # add explicit type symbol
+          n[^2] = getTypeInst n[^1]
+        n[^1] = normalizingRewrites n[^1]
         result = n
 
     proc rewriteVarLet(n: NimNode): NimNode =
@@ -100,9 +100,21 @@ proc normalizingRewrites*(n: NimNode): NimNode =
                 child
           of nnkIdentDefs:
             # a new section with a single rewritten identdefs within
-            result.add:
-              newNimNode(n.kind, n).add:
-                rewriteIdentDefs child
+            # result.add:
+            #   newNimNode(n.kind, n).add:
+            #     rewriteIdentDefs child
+            let
+              defs = rewriteIdentDefs(child)
+              definesMany = defs.len > 3
+            if definesMany:
+              for d in defs[0 .. ^3]: # last two nodes are type and rhs
+                result.add:
+                  newNimNode(n.kind, n).add:
+                    newIdentDefs(d, copyNimTree(defs[^2]), copyNimTree(defs[^1]))
+            else:
+              result.add:
+                newNimNode(n.kind, n).add:
+                  defs
           else:
             result.add:
               child.errorAst "unexpected"
@@ -153,6 +165,42 @@ proc normalizingRewrites*(n: NimNode): NimNode =
         else:
           result = n
       else: discard
+
+    proc rewriteFormalParams(n: NimNode): NimNode =
+      ## make formal params such as `foo(a, b: int)` into `foo(a: int, b: int)`
+      echo "before ", treeRepr n
+      case n.kind
+      of nnkFormalParams:
+        result = nnkFormalParams.newNimNode(n)
+        result.add:
+          copyNimNode(n[0]) # return value
+        for arg in n[1 .. ^1]:
+          case arg.kind
+          of nnkIdentDefs:
+            # if there is more than one param defined, then break them up
+            let
+              defs = rewriteIdentDefs(arg)
+              definesMany = defs.len > 3
+            if definesMany:
+              for d in defs[0 .. ^3]: # last two nodes are type and rhs
+                result.add:
+                  newIdentDefs(d, copyNimTree(defs[^2]), copyNimTree(defs[^1]))
+            else:
+              result.add:
+                defs
+          of nnkEmpty:
+            result.add:
+              arg.errorAst "unexpected"
+            echo "empty ", treeRepr n
+          else:
+            result.add:
+              # sometimes we have symbols, they get desymed elsewhere
+              normalizingRewrites arg
+            echo "else ", treeRepr n
+      else:
+        discard
+      echo "after ", treeRepr result
+      return result
 
     proc rewriteHidden(n: NimNode): NimNode =
       ## Unwrap hidden conversion nodes
@@ -207,6 +255,8 @@ proc normalizingRewrites*(n: NimNode): NimNode =
       rewriteConv n
     of nnkReturnStmt:
       rewriteReturn n
+    # of nnkFormalParams:
+    #   rewriteFormalParams n
     of CallNodes:
       rewriteHidden n
     else:
