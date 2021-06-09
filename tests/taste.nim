527a92ece39c2a1f4b4f5db32743c1f6b81a93bc
Leorize <leorize+oss@disroot.org>
2021-06-08 23:12:13 -0500

    tests/taste: move tests from thidden to taste

--- tests/taste.nim
+++ tests/taste.nim
@@ -449,3 +449,34 @@ suite "tasteful tests":
     var c = whelp foo(5)
     trampoline c
     check r == 2
+
+  block:
+    ## implicit up-casting
+    type O = ref object of RootObj
+
+    var r = 0
+
+    proc foo() {.cps: Continuation.} =
+      inc r
+      let o: RootRef = new O
+
+    foo()
+
+    check r == 1
+
+  block:
+    ## implicit conversion between non-object types
+    var r = 0
+
+    proc foo() {.cps: Continuation.} =
+      inc r
+      let n: Natural = 1
+      # implicit conversion in decl
+      let i: int = n
+      # implicit conversion in assignment
+      var j: int
+      j = n
+
+    foo()
+
+    check r == 1
