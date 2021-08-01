2e92115124329be9c3af81a04fe42a23c43c40dc
Andy Davidoff <github@andy.disruptek.com>
2021-07-31 23:05:01 -0400

    0.3.1: support danger 😎

--- cps.nim
+++ cps.nim
@@ -264,10 +264,11 @@ proc writeTraceDeque*(c: Continuation) {.cpsVoodoo.} =
   ## write a traceback for the current continuation
   if c.isNil:
     stdmsg().writeLine "dismissed continuations have no trace deque"
-  elif not cpsHasTraceDeque:
-    stdmsg().writeLine "compile with --define:cpsHasTraceDeque=on"
   else:
-    for index in 0 ..< c.frames.len:
-      template frame: TraceFrame = c.frames[c.frames.len - index - 1]
-      stdmsg().writeLine:
-        &"{frame.info.filename}({frame.info.line}) {frame.fun} <{frame.hook}>"
+    when not cpsHasTraceDeque:
+      stdmsg().writeLine "compile with --define:cpsHasTraceDeque=on"
+    else:
+      for index in 0 ..< c.frames.len:
+        template frame: TraceFrame = c.frames[c.frames.len - index - 1]
+        stdmsg().writeLine:
+          &"{frame.info.filename}({frame.info.line}) {frame.fun} <{frame.hook}>"
