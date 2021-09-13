b3c015cbc3f0c204b805fc74e5d6e20c6d6a112b
Leorize <leorize+oss@disroot.org>
2021-09-12 19:00:00 -0500

    call with the continuation's type instead of the magic type
    The fix to hooks in 236a74096027028ed908bd97ae9bf53976bc24ef solved
    the issue of getting the user type in other phases, as such we no longer
    need to extract it from magic/voodoo implementation.
    
    Fixes #233.

--- tests/tapi.nim
+++ tests/tapi.nim
@@ -148,3 +148,67 @@ suite "cps api":
       step 3
 
     foo()
+
+  block:
+    ## calling magic that is not defined for the base type should not compile
+    skip "FIXME: Figure out a way to test compile-time failures":
+      type AnotherCont = ref object of Continuation
+      proc magic(c: AnotherCont): AnotherCont {.cpsMagic.} = discard
+
+      proc foo() {.cps: Cont.} =
+        magic()
+
+  block:
+    ## calling magic/voodoo with generics continuation parameter works
+    # XXX: Not sure why, but Killer doesn't work here, complaining about
+    #      `k` not in scope.
+    var r = 0
+
+    type AnotherCont = ref object of Continuation
+    proc magic(c: Cont or AnotherCont): auto {.cpsMagic.} =
+      inc r
+      c
+
+    proc voodoo(c: Cont or AnotherCont) {.cpsVoodoo.} =
+      inc r
+
+    proc foo() {.cps: Cont.} =
+      inc r
+      voodoo()
+      magic()
+
+    foo()
+    check r == 3
+
+  # Not using block here since using can only be used on the top-level
+  using c: Cont
+
+  block:
+    ## magic/voodoo can be defined with `using`
+    var k = newKiller 4
+
+    proc magic(c): Cont {.cpsMagic.} =
+      step 2
+      check not c.dismissed, "continuation was dismissed"
+      check not c.finished, "continuation was finished"
+      result = c
+
+    proc voodoo(c): int {.cpsVoodoo.} =
+      step 3
+      check not c.dismissed, "continuation was dismissed"
+      check not c.finished, "continuation was finished"
+      result = 2
+
+    proc foo(): int {.cps: Cont.} =
+      noop()
+      step 1
+      check k.step == 1, "right"
+      magic()
+      check k.step == 2, "magic failed to run"
+      noop()
+      check voodoo() == 2, "voodoo failed to yield 2"
+      noop()
+      step 4
+      return 3
+
+    check foo() == 3
