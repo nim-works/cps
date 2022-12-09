47bb76fb1ee63efd0bad9433d5b830cf831b2315
Smooth Operator <github@andy.disruptek.com>
2022-12-09 17:45:56 -0500

    rm warning

--- cps/returns.nim
+++ cps/returns.nim
@@ -58,7 +58,6 @@ proc terminator*(c: Name; contType: Name; tipe: NormNode): NormNode =
   ## produce the terminating return statement of the continuation;
   ## this should return control to the mom and dealloc the continuation,
   ## or simply set the fn to nil and return the continuation.
-  let coop = NimNode hook(Coop, asName"result")
   let pass = NimNode hook(Pass, newCall(contType, c), c.dot "mom")
   let dealloc = NimNode hook(Dealloc, newCall(contType, c), tipe)
   let c = NimNode c
