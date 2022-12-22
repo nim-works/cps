e6c69e35aa93ac958af0f2b4751cee03b3d6d69f
Smooth Operator <github@andy.disruptek.com>
2022-12-22 15:05:57 -0500

    satisfy grinds

--- examples/coroutine.nim
+++ examples/coroutine.nim
@@ -4,44 +4,47 @@ import cps, std/sugar
 type
   Coroutine = ref object of Continuation
     data: int
-    next: Coroutine
+    suspended: Coroutine
 
 # Used to both launch and continue the execution of coroutines
-template resume(c: Coroutine): Coroutine =
-  trampoline c
+template resume(c: Coroutine): untyped =
+  discard trampoline c
 
 proc recv(c: Coroutine): int {.cpsVoodoo.} =
   c.data
 
 # Suspend execution of the coroutine
-proc jield(c: Coroutine): Coroutine {.cpsMagic.} =
-  c.next = c
-  return nil
+proc suspend(c: Coroutine): Coroutine {.cpsMagic.} =
+  c.suspended = c
 
 proc send(c: Coroutine, n: int) =
   c.data = n
-  discard c.next.resume()
+  resume c.suspended
 
 # This coroutine receives the data, applies f and sends the result to consumer
 proc filter(dest: Coroutine, f: proc(x: int): int) {.cps:Coroutine.} =
   while true:
-    jield()
+    suspend()
     let n = f(recv())
     dest.send(n)
 
 # This coroutine receives ints through filter and prints them
 proc consumer() {.cps:Coroutine.} =
   while true:
-    jield()
+    suspend()
     let value = recv()
     echo value
 
 let coro2 = whelp consumer()
 let coro1 = whelp filter(coro2, x => x * 2)
 
-discard coro1.resume()
-discard coro2.resume()
+resume coro1
+resume coro2
 
 # This prints numbers from 2 to 20 in 2 increment.
 for i in 1..10:
   coro1.send(i)
+
+# break the cycles
+reset coro1.suspended
+reset coro2.suspended
