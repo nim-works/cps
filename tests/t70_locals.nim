38d364185b7f3ad77928c61cc799128a1e9fd329
Smooth Operator <github@andy.disruptek.com>
2024-01-31 08:51:20 -0500

    fix sugar

--- tests/t70_locals.nim
+++ tests/t70_locals.nim
@@ -501,3 +501,22 @@ suite "lifetimes":
       # destroy bar.m; eg. step == 9
 
     foo()
+
+import std/sugar
+
+suite "high-cal":
+  var r = 0
+  block:
+    ## sugary procedure arguments can be used in expressions
+    r = 0
+    proc bar(x: int): int {.cps: Cont.} =
+      inc r
+      result = x * 2
+
+    proc foo(fn: (int) -> int): int {.cps: Cont.} =
+      inc r
+      result = fn: bar(2)
+      inc r
+
+    check 12 == foo(x => x * 3)
+    check r == 3
